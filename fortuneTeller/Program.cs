84ce83ec2c85f01ad1ca491830a51523cf64082d
Tatiane Troxell <tatianetroxell@gmail.com>
2017-01-07 23:40:43 -0500

    Initialized variables, insert questions for the user

--- fortuneTeller/Program.cs
+++ fortuneTeller/Program.cs
@@ -10,6 +10,38 @@ namespace fortuneTeller
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Birth Month (Please enter a number): ");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Favorite ROYGBIV color (Please enter \"HELP\" for a list of the ROYGBIV colors: ");
+            string colorAnswer = Console.ReadLine();
+            string color;
+
+
+
+            if (colorAnswer == "help")
+            {
+                Console.WriteLine("The ROYGBIV colors are:\n red\n orange\n yellow\n gree\n blue\n indigo\n violet");
+                Console.WriteLine("Please enter your favorite: ");
+                color = Console.ReadLine();
+            }
+            else
+            {
+                color = colorAnswer;
+            }
+
+            Console.WriteLine("How many siblings to you have: ");
+            int siblings = int.Parse(Console.ReadLine());
+
         }
     }
 }
